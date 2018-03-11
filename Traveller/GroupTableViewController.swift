64c7fca07a489c179f3383528c313d8510e3a99f
Idan Ben Zvi <darkidan@bmw09.cs.colman.ac.il>
2018-03-11 14:30:03 +0200

    created groups page

--- Traveller/GroupTableViewController.swift
+++ Traveller/GroupTableViewController.swift
@@ -1,10 +1,4 @@
-//
-//  GroupTableViewController.swift
-//  Traveller
-//
-//  Created by sapir shloush on 16.1.2018.
-//  Copyright © 2018 Traveller52. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
