b44bd6e204060197f995e94f359c41f43c3fd67b
Tal <talsahar73@gmail.com>
2018-03-13 19:38:42 +0200

    user's location  detection added

---
+++ Traveller/Model/AuthenticationModel.swift
@@ -0,0 +1,55 @@
+//
+//  AuthenticationModel.swift
+//  TheBigShame
+//
+//  Created by admin on 11/02/2018.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+class AuthenticationModel{
+    
+    
+    static func login(email: String, password: String, onSuccess: @escaping (_ user:User)->Void, onFail: @escaping (_ error:Error)->Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            (user != nil) ? onSuccess(user!) : onFail(error!)
+        }
+    }
+    
+    static func register(email: String, password: String,nickname:String, onSuccess: @escaping(_ user:User)->Void, onFail: @escaping (_ error:Error)->Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            (user != nil) ? updateProfile(displayName: nickname, photo: nil, thenDo: {onSuccess(user!)}) : onFail(error!)
+        }
+    }
+    
+    static func logout() {
+        try? Auth.auth().signOut()
+    }
+    
+    static func getCurrentUser()->User?{
+        return Auth.auth().currentUser
+    }
+    
+    
+    
+    
+    static func updateProfile(displayName:String?,photo:URL?,thenDo:@escaping ()->Void){
+        if let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest(){
+          
+            !(displayName?.isEmpty)! ? changeRequest.displayName = displayName : nil
+         
+            photo != nil ? changeRequest.photoURL = photo : nil
+            
+            changeRequest.commitChanges { (error) in
+                thenDo()
+            }
+        }
+    }
+    
+    static func isAdministrator()->Bool{
+        return AuthenticationModel.getCurrentUser()?.email == "talsahar73@gmail.com"
+    }
+    
+    
+}
