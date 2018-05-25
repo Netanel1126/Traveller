eeebbcc4df0fa6b480341e6d348e7e0c39624eda
Netanel1126 <30606968+Netanel1126@users.noreply.github.com>
2018-05-25 17:29:47 +0300

    Add files via upload

---
+++ Traveller/model/NotificationModel.swift
@@ -0,0 +1,50 @@
+//
+//  NotificationModel.swift
+//  Traveller
+//
+//  Created by Tal Sahar on 12/04/2018.
+//  Copyright © 2018 Traveller52. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ModelNotificationBase<T>{
+    var name:String?
+    
+    init(name:String){
+        self.name = name
+    }
+    
+    func observe(callback:@escaping (T?)->Void)->Any{
+        return NotificationCenter.default.addObserver(forName: NSNotification.Name(name!), object: nil, queue: nil) { (data) in
+            if let data = data.userInfo?["data"] as? T {
+                callback(data)
+            }
+        }
+    }
+    
+    func post(data:T?){
+        NotificationCenter.default.post(name: NSNotification.Name(name!), object: self, userInfo: ["data":data])
+    }
+}
+
+class TravellerNotification {
+
+    static let GetMapNotification = ModelNotificationBase<[Position]>(name: "GetMapNotification") //Map draw
+    
+    //model notifications
+    static let travellerUserNotification = ModelNotificationBase<Void>(name: "travellerUserDBNotification")
+    static let tripNotification = ModelNotificationBase<Void>(name: "tripNotification")
+    static let groupNotification = ModelNotificationBase<Void>(name: "groupNotification")
+    static let tripUsersNotification = ModelNotificationBase<Void>(name: "tripUsersNotification")
+    //server
+    static let serverChatNotification = ModelNotificationBase<MessageResponse>(name: "serverChatNotification")
+    static let localChatNotification = ModelNotificationBase<Void>(name: "localChatNotification")
+    static let serverCoordinateNotification = ModelNotificationBase<CoordinateResponse>(name: "serverCoordinateNotification")
+    static let localCoordinateNotification = ModelNotificationBase<Void>(name: "localCoordinateNotification")
+    
+    static func removeObserver(observer:Any){
+        NotificationCenter.default.removeObserver(observer)
+    }
+}
